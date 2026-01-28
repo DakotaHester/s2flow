f65753f61723eee83f264982eba7938dcfe0ec10
Dakota Hester <admin@dakotahester.com>
2026-01-28 15:21:24 -0600

    Lots of new figures

--- src/s2flow/data/utils.py
+++ src/s2flow/data/utils.py
@@ -1,9 +1,9 @@
 import torch
 from typing import Tuple, Union
-
+import numpy as np
 
 def scale(
-        data, 
+        data: Union[torch.Tensor, np.ndarray],
         in_range: Union[Tuple[int, int], Tuple[float, float]]=(0, 10000), 
         out_range: Union[Tuple[int, int], Tuple[float, float]]=(-1.0, 1.0)
 ) -> torch.Tensor:
@@ -13,5 +13,12 @@ def scale(
     
     # scale to out_range
     data = data * (out_range[1] - out_range[0]) + out_range[0]
-    data = data.clamp(min=out_range[0], max=out_range[1])
+    
+    # if torch tensor, clamp values
+    if isinstance(data, torch.Tensor):
+        data = data.clamp(min=out_range[0], max=out_range[1])
+    elif isinstance(data, np.ndarray):
+        data = np.clip(data, out_range[0], out_range[1])
+    else:
+        raise TypeError("data must be a torch.Tensor or numpy.ndarray")
     return data
