c18d151f828e3ef3eecba5da9eb677f2334db676
Dakota Hester <admin@dakotahester.com>
2026-02-11 14:01:49 -0600

    Tweak HPC settings for scale

--- slurm/sr_inference_sweep.py
+++ slurm/sr_inference_sweep.py
@@ -5,7 +5,7 @@ from s2flow.slurm import BaseJob, BaseSweep, SlurmConfig
 
 def main() -> None:
     slurm_config = SlurmConfig(
-        memory='16G',
+        memory='32G',
         partition='gpu-a100-mig2',
         gres='gpu:a100_3g.40gb',
         n_tasks=4,
