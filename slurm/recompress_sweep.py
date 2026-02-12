031e986bad99d50f6a58a7657866782ea99e0c7d
Dakota Hester <admin@dakotahester.com>
2026-02-12 10:35:47 -0600

    Recompression code

--- slurm/recompress_sweep.py
+++ slurm/recompress_sweep.py
@@ -22,7 +22,7 @@ class RecompressJob(BaseJob):
     def _update_config(self):
         # This sweep doesn't use a YAML config, but the base class 
         # may require this to be implemented. 
-        pass
+        self.base_config['job']['name'] = self._generate_job_name()
 
     def _generate_job_name(self) -> str:
         # Sanitize filename for SLURM job name
